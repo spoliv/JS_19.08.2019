6a5e6f361f9204a17d2e53342491f3e63d2c4487
OlegSP <spoliv@rambler.ru>
2019-09-01 23:57:11 +0300

    DZ_4

--- dz4.js
+++ dz4.js
@@ -2,51 +2,72 @@
 
 // Задание 1
 
-var num = +parseInt(prompt('Введите число от 0 до 999'));
-var number = {};
-
-function convertNumToObject(num) {
-    if (num > 999){
-        console.log('Вы ввели недопустимое число')
-    }
-    else if (100 <= num <= 999) {
-        number['единицы'] = (num %100) % 10;
-        number['десятки'] = (num % 100 - (num %100) % 10) / 10;
-        number['сотни'] = (num - num % 100) / 100;
-    }
-    else if (10 <= num < 100) {
-        number['единицы'] = num % 10;
-        number['десятки'] = (num - num % 10) / 10;
-        number['сотни'] = 0;
-    }
-
-    else if (0 <= num < 10) {
-        number['единицы'] = num % 10;
-        number['десятки'] = 0;
-        number['сотни'] = 0;
-    }
-    return number;
-}
-
-console.log(convertNumToObject(num));
+// var num = +parseInt(prompt('Введите число от 0 до 999'));
+// var number = {};
+//
+// function convertNumToObject(num) {
+//     if (num > 999){
+//         console.log('Вы ввели недопустимое число')
+//     }
+//     else if (100 <= num <= 999) {
+//         number['единицы'] = (num %100) % 10;
+//         number['десятки'] = (num % 100 - (num %100) % 10) / 10;
+//         number['сотни'] = (num - num % 100) / 100;
+//     }
+//     else if (10 <= num < 100) {
+//         number['единицы'] = num % 10;
+//         number['десятки'] = (num - num % 10) / 10;
+//         number['сотни'] = 0;
+//     }
+//
+//     else if (0 <= num < 10) {
+//         number['единицы'] = num % 10;
+//         number['десятки'] = 0;
+//         number['сотни'] = 0;
+//     }
+//     return number;
+// }
+//
+// console.log(convertNumToObject(num));
 
 
 // Задание 2
 
-//var basket = [];
+/* Теперь корзина представляет собой массив, каждый элемент которого - объект
+   со свойствами name, price, amount
+*/
+
+// var basket = [];
 //
 // function countBasketPrice(basket){
 //     let basketValue = 0;
-//     for (let i = 0; i < basket.length; i++) {
-//         basketValue += basket[i][1] * basket[i][2]
+//     for (let product of basket) {
+//         basketValue += product['price'] * product['amount'];
 //     }
 //     return basketValue;
 // }
-
+//
 // basket = [
-//            ['product1', 1000, 3],
-//            ['product2', 1200, 4],
-//            ['product3', 500, 9]
+//     {
+//         'name': 'product1',
+//         'price': 1000,
+//         'amount': 3
+//     },
+//
+//     {
+//         'name': 'product2',
+//         'price': 100,
+//         'amount': 6
+//
+//     },
+//
+//     {
+//         'name': 'product3',
+//         'price': 25,
+//         'amount': 8
+//
+//     },
+//
 // ];
 //
 // console.log(countBasketPrice(basket));
