636e618d070ecbbeadfd37dd25b94f95e0d6e5f5
OlegSP <spoliv@rambler.ru>
2019-09-11 00:52:32 +0300

    ДЗ_6

--- DZ_6/dz6.js
+++ DZ_6/dz6.js
@@ -10,18 +10,37 @@ const catalog = {
     product,
     catalogItems: null,
 
-    addProductToCatalog(name, price, disc) {
-        this.catalogItems = [];
-        this.product[name] = name;
-        this.product[price] = price;
-        this.product[name] = disc;
-        this.catalogItems.push(this.product);
+    addProductToCatalog( name, price, disc) {
+        this.product.name = name;
+        this.product.price = price;
+        this.product.disc = disc;
+        this.render();
+    },
+    render() {
+        const divTag = document.createElement('div');
+
+        const p1Tag = document.createElement('p');
+        p1Tag.textContent = this.product.name;
+        divTag.appendChild(p1Tag);
+
+        const p2Tag = document.createElement('p');
+        p2Tag.textContent = `${this.product.price} руб.`;
+        divTag.appendChild(p2Tag);
+
+        const p3Tag = document.createElement('p');
+        p3Tag.textContent = this.product.disc;
+        divTag.appendChild(p3Tag);
+
+        const button = document.createElement('button');
+        button.innerText = 'Купить';
+        divTag.appendChild(button);
+
+        document.body.appendChild(divTag);
     }
 };
 
+// Наполнение каталога
 
-catalog.addProductToCatalog('утюг', '1500', 'бытовой прибор');
+catalog.addProductToCatalog('утюг', '1500', 'электрический прибор');
 catalog.addProductToCatalog('пылесос', '4500', 'бытовой прибор');
-catalog.addProductToCatalog('светильник', '1500', 'бытовой прибор');
-
-console.log(catalog.catalogItems);
+catalog.addProductToCatalog('светильник', '1500', 'прибор освещения');
